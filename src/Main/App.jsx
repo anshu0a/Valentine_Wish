8bafbe079b6e525189e9ee37fb8c83d46ff2f245
Anshu Kumar <anshu0a@gmail.com>
2026-02-06 00:16:15 +0530

    idn

--- src/Main/App.jsx
+++ src/Main/App.jsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../JsxFile/Home/MainHome.jsx";
-import Add from '../JsxFile/Add/MainAdd.jsx'
-import View from "../JsxFile/view/mainView.jsx"
-import NotFound from '../JsxFile/Help/NotFound.jsx'
+import Add from "../JsxFile/Add/MainAdd.jsx";
+import View from "../JsxFile/view/mainView.jsx";
+import NotFound from "../JsxFile/Help/NotFound.jsx";
+
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-       <Route path="/register" element={<Add />} />
-       <Route path="/view/:id" element={<View />} />
-        <Route path="/*" element={<NotFound />} />
+      <Route path="/register" element={<Add />} />
+      <Route path="/view/:id" element={<View />} />
+      <Route path="/*" element={<NotFound />} />
     </Routes>
   );
 }
